c3ab888a466f42f1ce8e728c8274eef312d6b9df
Marcus Lassila <lassila@chalmers.se>
2024-04-26 15:56:39 +0200

    Target model training

--- src/engine.py
+++ src/engine.py
@@ -1,20 +1,44 @@
+import torch
 from tqdm.auto import tqdm
 from collections import defaultdict
 
-def train_step(model, train_loader, loss_fn, optimizer, device):
+def train_step(model, data_loader, loss_fn, optimizer, criterion, device):
     model.train()
-    for batch in train_loader:
+    avg_loss, avg_score = 0, 0
+    for batch in data_loader:
         batch.to(device)
         optimizer.zero_grad()
         out = model(batch.x, batch.edge_index)
-        loss = loss_fn(out, batch.y)
+        loss = loss_fn(out[batch.train_mask], batch.y[batch.train_mask])
+        avg_loss += loss.item()
+        avg_score += criterion(out[batch.train_mask].argmax(dim=1), batch.y[batch.train_mask])
         loss.backward()
         optimizer.step()
-    return loss.item()
+    avg_loss /= len(data_loader)
+    avg_score /= len(data_loader)
+    return avg_loss, avg_score
 
-def train(model, train_loader, valid_loader, loss_fn, optimizer, epochs, device):
+def valid_step(model, data_loader, loss_fn, criterion, device):
+    model.eval()
+    avg_loss, avg_score = 0, 0
+    with torch.inference_mode():
+        for batch in data_loader:
+            batch.to(device)
+            out = model(batch.x, batch.edge_index)
+            loss = loss_fn(out[batch.val_mask], batch.y[batch.val_mask])
+            avg_loss += loss
+            avg_score += criterion(out[batch.val_mask].argmax(dim=1), batch.y[batch.val_mask])
+        avg_loss /= len(data_loader)
+        avg_score /= len(data_loader)
+    return avg_loss, avg_score
+
+def train(model, data_loader, loss_fn, optimizer, criterion, epochs, device):
     res = defaultdict(list)
     for _ in tqdm(range(epochs), desc=f"Training on {device}."):
-        loss = train_step(model, train_loader, loss_fn, optimizer, device)
-        res['loss'].append(loss)
+        train_loss, train_score = train_step(model, data_loader, loss_fn, optimizer, criterion, device)
+        res['train_loss'].append(train_loss)
+        res['train_score'].append(train_score)
+        valid_loss, valid_score = valid_step(model, data_loader, loss_fn, criterion, device)
+        res['valid_loss'].append(valid_loss)
+        res['valid_score'].append(valid_score)
     return res
--- src/train_target.py
+++ src/train_target.py
@@ -8,27 +8,41 @@ import torch
 import torch.nn.functional as F
 import torch_geometric
 from torch_geometric.loader import DataLoader
+from torchmetrics import Accuracy
 
 parser = argparse.ArgumentParser()
 parser.add_argument("--ds", default='Cora', type=str)
+parser.add_argument("--batch-size", default=32, type=int)
+parser.add_argument("--epochs", default=50, type=int)
+parser.add_argument("--lr", default=1e-3, type=float)
 args = parser.parse_args()
 
 class Config:
-    batch_size = 32
-    epochs     = 50
+    batch_size = args.batch_size
+    epochs     = args.epochs
     device     = "cuda" if torch.cuda.is_available() else "cpu"
     root       = "./data"
     hidden_dim = 256
-    lr         = 1e-3
+    lr         = args.lr
+    
+    @staticmethod
+    def print():
+        print("\n########## Config ##########")
+        print(f"Batch size: {Config.batch_size}")
+        print(f"Epochs: {Config.epochs}")
+        print(f"Learning rate: {Config.lr}")
+        print(f"Device: {Config.device}")
+        print(f"Hidden dimension: {Config.hidden_dim}")
+        print("############################\n")
 
 def get_dataset():
     if args.ds == "Cora":
-        dataset = torch_geometric.datasets.CoraFull(Config.root + "/Cora")
+        dataset = torch_geometric.datasets.Planetoid(root=Config.root, name="Cora", split="random", num_train_per_class=90)
     else:
         raise ValueError("Unsupported dataset!")
     return dataset
 
-def run():
+def main():
     dataset = get_dataset()
     model = models.GCN(
         dataset.num_features,
@@ -36,11 +50,13 @@ def run():
         dataset.num_classes
     ).to(Config.device)
 
-    train_loader = DataLoader(dataset, batch_size=Config.batch_size, shuffle=True)
+    data_loader = DataLoader(dataset, batch_size=Config.batch_size, shuffle=True)
     optimizer = torch.optim.Adam(model.parameters(), lr=Config.lr)
+    criterion = Accuracy(task='multiclass', num_classes=dataset.num_classes)
     loss_fn = F.nll_loss
-    res = engine.train(model, train_loader, None, loss_fn, optimizer, Config.epochs, Config.device)
+    res = engine.train(model, data_loader, loss_fn, optimizer, criterion, Config.epochs, Config.device)
     utils.plot_training_results(res)
 
 if __name__ == '__main__':
-    run()
+    Config.print()
+    main()
