Improve information leakage computation. Add test for tpr at fixed fpr. New config file for default parameters.

src/lood.py
@@ -53,10 +53,11 @@ class LOOD:
         config = self.config
 
         preds = []
+        edge_index = torch.tensor([[],[]], dtype=torch.bool)
         for _ in tqdm(range(5000), desc="Training shadow models"):
             model = self.train_model(dataset, hidden_dims=[64], dropout=0.0)
-            with torch.no_grad():
-                preds.append(model(dataset.x, dataset.edge_index)[0, dataset.y[0]])
+            with torch.inference_mode():
+                preds.append(model(dataset.x, edge_index)[0, dataset.y[0]])
         preds = torch.tensor(preds)
         mean = preds.mean()
         std = preds.std()
@@ -65,12 +66,25 @@ class LOOD:
         utils.plot_histogram_and_fitted_gaussian(preds, mean, std, bins=50, savepath=f'{config.savedir}/retrain.png')
 
         preds = []
+        edge_index = torch.tensor([[],[]], dtype=torch.bool)
+        for _ in tqdm(range(50), desc="Training shadow models"):
+            model = self.train_model(dataset, hidden_dims=[64], dropout=0.0)
+            with torch.inference_mode():
+                preds.append(model(dataset.x, edge_index)[0, dataset.y[0]])
+        preds = torch.tensor(preds)
+        mean = preds.mean()
+        std = preds.std()
+        print('Retrain budget')
+        print(f'mean {mean:.4f}, std: {std:.4f}')
+        utils.plot_histogram_and_fitted_gaussian(preds, mean, std, bins=50, savepath=f'{config.savedir}/retrain_budget.png')
+
+        preds = []
         for _ in tqdm(range(50), desc="Training shadow models"):
             model = self.train_model(dataset, hidden_dims=[128], dropout=0.5)
             model.dropout_during_inference = True
-            with torch.no_grad():
+            with torch.inference_mode():
                 for _ in range(1000):
-                    preds.append(model(dataset.x, dataset.edge_index)[0, dataset.y[0]])
+                    preds.append(model(dataset.x, edge_index)[0, dataset.y[0]])
         preds = torch.tensor(preds)
         mean = preds.mean()
         std = preds.std()
@@ -82,9 +96,9 @@ class LOOD:
         model.dropout_during_inference = True
     
         preds = []
-        with torch.no_grad():
+        with torch.inference_mode():
             for _ in range(10000):
-                preds.append(model(dataset.x, dataset.edge_index)[0, dataset.y[0]])
+                preds.append(model(dataset.x, edge_index)[0, dataset.y[0]])
         preds = torch.tensor(preds)
         mean = preds.mean()
         std = preds.std()
@@ -92,40 +106,40 @@ class LOOD:
         print(f'mean {mean:.4f}, std: {std:.4f}')
         utils.plot_histogram_and_fitted_gaussian(preds, mean, std, bins=50, savepath=f'{config.savedir}/mc_dropout.png')
 
-    def information_leakage(self, dataset, num_shadow_models=50, mc_dropout_samples=1000):
-        hidden_dims = [2 * x for x in self.config.hidden_dim_target]
+    def information_leakage(self, dataset, node_index, num_shadow_models=50):
+        hidden_dims = self.config.hidden_dim_target
         dropout = 0.5
-        train_set = datasetup.masked_subgraph(dataset, dataset.train_mask)
-        test_set = datasetup.masked_subgraph(dataset, dataset.test_mask)
-        node_index = torch.arange(train_set.num_nodes)[:2]
+        empty_edge_index = torch.tensor([[],[]], dtype=torch.long)
         incl_preds = []
         for _ in tqdm(range(num_shadow_models), desc='Training inclusion models'):
             incl_model = self.train_model(dataset, hidden_dims=hidden_dims, dropout=dropout)
-            with torch.no_grad():
-                for _ in range(mc_dropout_samples):
-                    pred = incl_model(train_set.x, train_set.edge_index)[node_index, train_set.y[node_index]]
-                    incl_preds.append(pred)
+            with torch.inference_mode():
+                pred = incl_model(dataset.x, empty_edge_index)[node_index, dataset.y[node_index]]
+                incl_preds.append(pred)
         incl_preds = torch.stack(incl_preds)
         incl_means = incl_preds.mean(dim=0)
         incl_stds = incl_preds.std(dim=0)
+        assert incl_preds.shape == (num_shadow_models, node_index.shape[0])
+        assert incl_means.shape == node_index.shape
 
         excl_preds = []
         for node in node_index:
             preds = []
-            sub_node_index = torch.cat((node_index[:node], node_index[node + 1:]))
-            sub_train_set = datasetup.masked_subgraph(train_set, sub_node_index)
-            sub_graph = datasetup.merge_graphs(sub_train_set, test_set)
+            mask = torch.ones(dataset.num_nodes, dtype=torch.bool)
+            mask[node] = False
+            sub_dataset= datasetup.masked_subgraph(dataset, mask)
             for _ in tqdm(range(num_shadow_models), desc='Training exclusion models'):
-                excl_model = self.train_model(sub_graph, hidden_dims=hidden_dims, dropout=dropout)
-                with torch.no_grad():
-                    for _ in range(mc_dropout_samples):
-                        pred = excl_model(train_set.x, train_set.edge_index)[node, train_set.y[node]]
-                        preds.append(pred)
+                excl_model = self.train_model(sub_dataset, hidden_dims=hidden_dims, dropout=dropout)
+                with torch.inference_mode():
+                    pred = excl_model(sub_dataset.x, empty_edge_index)[node, dataset.y[node]]
+                    preds.append(pred)
             preds = torch.tensor(preds)
             excl_preds.append(preds)
         excl_preds = torch.stack(excl_preds)
         excl_means = excl_preds.mean(dim=1)
         excl_stds = excl_preds.std(dim=1)
+        assert excl_preds.shape == (node_index.shape[0], num_shadow_models)
+        assert excl_means.shape == node_index.shape
 
         info_leakage = gaussian_kl_divergence(incl_means, incl_stds, excl_means, excl_stds)
         return info_leakage

src/utils.py
@@ -92,13 +92,9 @@ def graph_info(dataset):
 
 def tpr_at_fixed_fpr(fpr, tpr, target_fpr, thresholds):
     idx = np.argmax(fpr >= target_fpr)
-    if fpr[idx] != target_fpr:
-        x0, x1 = fpr[idx - 1], fpr[idx]
-        y0, y1 = tpr[idx - 1], tpr[idx]
-        slope = (y1 - y0) / (x1 - x0)
-        return slope * (target_fpr - x0) + y0, thresholds[idx - 1]
-    else:
-        return tpr[idx], thresholds[idx]
+    if fpr[idx] > target_fpr + 1e-6:
+        idx -= 1
+    return tpr[idx], thresholds[idx]
 
 def tpr_at_fixed_fpr_multi(soft_preds, truth, target_fpr):
     truth = truth.bool()
@@ -262,9 +258,7 @@ def plot_hinge_histogram(hinge, label_mask, train_mask, savepath=None):
     savefig_or_show(savepath)
 
 def plot_leakage_scatter(attack_preds, info_leakage, savepath=None):
-    indices = torch.arange(attack_preds.shape[0])
     plt.figure(figsize=(8, 8))
-    plt.scatter(indices, attack_preds, c='r', marker='x')
-    plt.scatter(indices, info_leakage, c='b', marker='o')
+    plt.scatter(attack_preds, info_leakage, c='r', marker='x')
     plt.grid(True)
     savefig_or_show(savepath)