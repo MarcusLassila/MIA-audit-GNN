25ef70727c439abd6f0a79e38c98e66e98cd9a82
Marcus Lassila <marcus@ducce.net>
2025-02-24 22:47:34 +0100

    Plot roc curve from csv

--- src/parse_roc_csv.py
+++ src/parse_roc_csv.py
@@ -5,10 +5,21 @@ def parse_roc_csv():
     roc_df = pd.read_csv('results/roc.csv', sep=',')
     fprs = roc_df.filter(like='FPR_', axis=1)
     tprs = roc_df.filter(like='TPR_', axis=1)
-    pd.set_option('display.max_columns', None)
-    pd.set_option('display.max_rows', None)
-    print(fprs)
-    print(tprs)
+
+    plt.figure(figsize=(12, 12))
+    legend = []
+    for (k1, v1), (k2, v2) in zip(fprs.items(), tprs.items()):
+        assert k1[4:] == k2[4:]
+        plt.loglog(v1.dropna(), v2.dropna())
+        legend.append(k1[4:])
+    plt.legend(legend)
+    plt.xlim(1e-4, 1)
+    plt.ylim(1e-4, 1)
+    plt.grid(True)
+    plt.xlabel('FPR')
+    plt.ylabel('TPR')
+    plt.title('roc_curve')
+    plt.savefig('./results/roc_curve.png')
 
 if __name__ == '__main__':
     parse_roc_csv()
